e2e5610a7e66ac41ae41cac3177f248676eeac1d
Jokodola Esther <estherjokodola21@gmail.com>
2026-03-20 08:10:33 +0100

    Built api endpoints

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "content": "FastAPI is awesome",
+        "title": "This framework is really easy to use and super fast",
+        "date_posted":"April 20, 2025",
+        
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "content": "I love FastAPI",
+        "title": "FastAPI is the best framework for building APIs",
+        "date_posted":"April 21, 2025",
+    }
+]  
+
+tab = FastAPI()
+
+@tab.get("/", response_class=HTMLResponse, include_in_schema=False)
+@tab.get("/posts", response_class=HTMLResponse, include_in_schema=False)
+def home():
+    return f"<h1>{posts[0]['title']}<h1>"
+
+@tab.get("/api/posts")
+def get_posts(): 
+    return posts
--- medicalapi_blog/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from medicalapi-blog!")
-
-
-if __name__ == "__main__":
-    main()
